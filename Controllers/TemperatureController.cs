313ff07d194c8d6580f623232485fadefd344a88
saritacutinha <37643735+saritacutinha@users.noreply.github.com>
2020-05-29 17:42:24 +0930

    Initial commit

---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TemperatureVisualiser.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+       private readonly ILogger<TemperatureController> _logger;
+
+        public TemperatureController(ILogger<TemperatureController> logger)
+        {
+            _logger = logger;
+        }       
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Temperature temperatureData)
+        {
+            DateTime date = temperatureData.Date;
+            int temperatureC = temperatureData.TemperatureC;
+            string summary = temperatureData.Summary;
+            return Ok();
+        }
+    }
+}
---
+++ Temperature.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TemperatureVisualiser
+{
+    public class Temperature
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
